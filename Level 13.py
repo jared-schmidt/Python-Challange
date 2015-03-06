26a220584dc433ed23d905befe78b24184450256
Jared Schmidt <jschmidt@problemsolutions.net>
2015-03-06 16:37:02 -0500

    lvl13

--- Level 13.py
+++ Level 13.py
@@ -9,12 +9,20 @@ from PIL import Image
 
 # FROM level 12
 # http://www.pythonchallenge.com/pc/return/evil4.jpg
-im = open('evil4.jpg')
-print(im.read()) # Bert is evil! go back!
-
+# im = open('evil4.jpg')
+# print(im.read()) # Bert is evil! go back! # http://www.pythonchallenge.com/pc/return/bert.html
+# im.close()
 # http://www.pythonchallenge.com/pc/return/remote.html
 # the phone is remote
 
+im = Image.open('bert.gif')
+im = im.convert('RGB')
+for y in range(im.size[1]):
+    for x in range(im.size[0]):
+        cood = (x, y)
+        p = im.getpixel(cood)
+        print(p)
+# print(im.tobytes())
 
 
 # phone that <remote /> evil
